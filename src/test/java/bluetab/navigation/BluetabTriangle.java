8910907b3eb0bac1a4345c16703618f436152506
edinson.quevedo <quevedo.edinson@gmail.com>
2025-08-12 11:02:38 -0500

    update url : 12-08-2024

--- src/test/java/bluetab/navigation/BluetabTriangle.java
+++ src/test/java/bluetab/navigation/BluetabTriangle.java
@@ -3,7 +3,7 @@ package bluetab.navigation;
 import net.serenitybdd.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
 
-@DefaultUrl("https://www.developsense.com/triangle/triangle.html")
+@DefaultUrl("https://www.developsense.com/triangle/tri.html")
 public class BluetabTriangle extends PageObject {
 
 }
