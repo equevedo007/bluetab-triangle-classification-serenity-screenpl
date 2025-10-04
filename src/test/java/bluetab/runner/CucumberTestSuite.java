14f50efe23ac560d85b777109673a44d30b0852a
EDINSON QUEVEDO <quevedo.edinson@gmail.com>
2025-10-03 19:57:18 -0500

    Configurar README.md 01

--- src/test/java/bluetab/runner/CucumberTestSuite.java
+++ src/test/java/bluetab/runner/CucumberTestSuite.java
@@ -1,9 +1,12 @@
 package bluetab.runner;
 
-
+import bluetab.utils.ReporteCucumber;
 import io.cucumber.junit.CucumberOptions;
+
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
 
 /**
  * Runner principal para ejecutar los escenarios etiquetados con @Triangulo.
@@ -14,8 +17,18 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "bluetab.stepdefinitions",
-        plugin = {"pretty", "json:target/cucumber-report.json"},
+        plugin = {"pretty",
+                "json:target/cucumber-report.json",
+                "json:target/cucumber-report/cucumber.json"},
         tags = "@Triangulo"
 )
 public class CucumberTestSuite {
+
+    @AfterClass
+    public static void generarReporteCucumber() {
+        ReporteCucumber reporte = new ReporteCucumber();
+        reporte.cucumberReport("target/cucumber-report");
+
+    }
+
 }
