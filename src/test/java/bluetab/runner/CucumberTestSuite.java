910c25985d5c642eb860c02f9fbd92f98b761bf1
edinson.quevedo <quevedo.edinson@gmail.com>
2026-02-27 12:19:22 -0500

    update Serenity BDD 5.3.2

--- src/test/java/bluetab/runner/CucumberTestSuite.java
+++ src/test/java/bluetab/runner/CucumberTestSuite.java
@@ -1,34 +1,22 @@
 package bluetab.runner;
 
-import bluetab.utils.ReporteCucumber;
-import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.AfterClass;
-import org.junit.runner.RunWith;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-
-/**
- * Runner principal para ejecutar los escenarios etiquetados con @Triangulo.
- * Utiliza Serenity BDD con Cucumber para generar reportes detallados.
- */
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "bluetab.stepdefinitions",
-        plugin = {"pretty",
-                "json:target/cucumber-report.json",
-                "json:target/cucumber-report/cucumber.json"},
-        tags = "@Triangulo"
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "bluetab.stepdefinitions")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "net.serenitybdd.cucumber.core.plugin.SerenityReporter," +
+                "pretty," +
+                "json:target/cucumber-report.json," +
+                "json:target/cucumber-report/cucumber.json"
 )
-public class CucumberTestSuite {
-
-    @AfterClass
-    public static void generarReporteCucumber() {
-        ReporteCucumber reporte = new ReporteCucumber();
-        reporte.cucumberReport("target/cucumber-report");
-
-    }
-
-}
+public class CucumberTestSuite { }
