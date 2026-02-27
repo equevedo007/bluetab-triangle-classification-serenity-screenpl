1d8b133f7e400420ba54ff316abd3d4b02e1a749
edinson.quevedo <quevedo.edinson@gmail.com>
2026-02-27 12:27:10 -0500

    update Serenity BDD 5.3.2 class runner

--- src/test/java/bluetab/runner/CucumberTestSuite.java
+++ src/test/java/bluetab/runner/CucumberTestSuite.java
@@ -3,6 +3,7 @@ package bluetab.runner;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
@@ -10,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("/features")
+@SelectPackages("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "bluetab.stepdefinitions")
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
